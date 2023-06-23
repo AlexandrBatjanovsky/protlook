4247c3ae03f345c1db06d78e60dc2f2c5d256aa7
AlexandrBatjanovsky <suner_s@mail.ru>
2023-06-23 15:25:43 +0300

    multy-download1

--- protlook.jl
+++ protlook.jl
@@ -1,11 +1,52 @@
+"""
+module PDBCIF_File  defines loading and reading
+CIF and PDB format modeles.
+"""
+# module PDBCIF_File
+
 using FilePaths ; using FilePathsBase: / ; using Glob
 using CodecZlib, TranscodingStreams
-using CrystalInfoFramework
+using DataFrames, CSV
+using ProgressMeter, Distributed
 
-protmodels_path = cwd()/"protlook"
-list_gf = glob("*.gz", protmodels_path)
-println("numstruct ", size(list_gf))
+@everywhere function downloadCif(PDBId::AbstractString, fname=missing, zip=true)
 
+    (fname === missing) && (fname = PDBId)
+    fname = PDBId*"."*"cif"
+    if zip
+        fname *=".gz"
+    end
+    if !ispath(pwd()*"/structs/"*fname)
+        download("https://files.rcsb.org/download/"*fname, pwd()*"/structs/"*fname)
+    end
+end
+
+framePDB = CSV.read("2", DataFrame; header=true)
+
+placedir = cwd()/"structs"
+
+if !exists(placedir)
+    mkdir(placedir, recursive=true)
+end
+cd(placedir)
+
+#println(typeof(unique(framePDB[!, :PDBId])[1]))
+#downloadCif(unique(framePDB[!, :PDBId])[1])
+
+pdb_download = unique(framePDB[!, :PDBId])
+@sync @distributed for pdbid in pdb_download
+    println(pdbid)
+    downloadCif(pdbid)
+end
+
+# for pdbid_loadcif in Set(framePDB[!, :PDBId])
+#     downloadCif(pdbid_loadcif)
+# end
+
+
+
+# end    # module PDBCIF_File
+"""
 t_single = t_all = time()
 for prot_gfile in list_gf
     kinanet=Cif(read(GzipDecompressorStream(open(prot_gfile)), String), verbose=false, version=2, source=p"/home/alexandersn/WORK/julia/protlook/100d-assembly1.cif.gz")
@@ -13,5 +54,6 @@ for prot_gfile in list_gf
     global t_single = time()
 end
 println("alltime:", time()-t_all)
+"""
 
 
---
+++ utils/createdataframe.jl
@@ -0,0 +1,8 @@
+# change list PDB files to PDB Frame
+# function getpdbid(stra)
+#    chop(stra, head=0, tail=17) # *"-"*chop(stra, head=13, tail=7)
+# end
+using DataFrames, CSV
+
+framePDB = CSV.read("1", DataFrame; header=false)
+CSV.write("2", hcat(select(framePDB, :Column1 => (x -> SubString.(x, Ref(1), Ref(4))) => :PDBId), rename(framePDB, 1=>:FileName)))
